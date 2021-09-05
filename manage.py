a7c766113e63e858be57bd35a6351362acf11c4e
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-05 23:56:13 +0300

    add save,delete, update image, filter by location, and search by category methods for the image model

--- manage.py
+++ manage.py
@@ -7,21 +7,21 @@ import dotenv
 
 
 def main():
-    """Run administrative tasks."""
+  """Run administrative tasks."""
 
-    dotenv.read_dotenv(override=True)
+  dotenv.read_dotenv(override=True)
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'igallery.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+  os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'igallery.settings')
+  try:
+    from django.core.management import execute_from_command_line
+  except ImportError as exc:
+    raise ImportError(
+			"Couldn't import Django. Are you sure it's installed and "
+			"available on your PYTHONPATH environment variable? Did you "
+			"forget to activate a virtual environment?"
+	) from exc
+  execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
-    main()
+  main()
--- wallpapers/models.py
+++ wallpapers/models.py
@@ -1,18 +1,44 @@
 from django.db import models
-import datetime as dt
 from django.utils import timezone
 
 # Create your models here.
 class Image(models.Model):
   image_name = models.CharField(max_length = 255)
   post_date = models.DateTimeField(auto_now_add=True)
-  image_description = models.CharField(max_length=255)
-  location = models.ForeignKey('Location', on_delete=models.CASCADE)
-  category = models.ForeignKey('Category', on_delete=models.CASCADE)
+  image_description = models.CharField(max_length=255, default=timezone.now)
+  location = models.ForeignKey('Location', on_delete=models.CASCADE, default=timezone.now)
+  category = models.ForeignKey('Category', on_delete=models.CASCADE, default=timezone.now)
   image_view = models.ImageField(upload_to = 'images/', default=timezone.now)
   
   def __str__(self):
     return self.image_name
+  
+  def save_image(self):
+    self.save()
+
+  def delete_image(self):
+    self.delete()
+
+
+  @classmethod
+  def search_by_category(cls,search_term):
+    search_result = cls.objects.filter(inage_category__wallpaper_name__icontains = search_term)
+    return search_result
+
+  @classmethod
+  def get_image_by_id(cls,incoming_id):
+    image_result = cls.objects.get(id=incoming_id)
+    return image_result
+
+  @classmethod
+  def updare_image(cls,current_value,new_value):
+    fetched_object = Image.objects.filter(image_name = current_value).update(image_name = new_value)
+    return fetched_object
+
+  @classmethod
+  def filter_by_location(cls,location):
+    filtered_result = cls.objects.filter(image_location__location_name__icontains = location)
+    return filtered_result
 
 
 class Category(models.Model):
