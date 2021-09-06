043bf200c39b08f124919e65fa16feb590bbcd68
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-06 22:38:02 +0300

    register models to allow operation with the django admin dashboard on the application

--- wallpapers/admin.py
+++ wallpapers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Image,Category,Location,Tag
 
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Tag)
+
--- wallpapers/urls.py
+++ wallpapers/urls.py
@@ -4,8 +4,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-  url(r'^$',views.welcome, name='welcome'),
-  url(r'^movies/', views.movie_wallpaper, name='movieWallpapers'),
+  url(r'^$',views.movie_wallpaper, name='movieWallpapers'),
   url(r'^sports/', views.sport_wallpaper, name='sportWallpapers'),
   url(r'^nature/', views.nature_wallpaper, name='natureWallpapers')
   ]
